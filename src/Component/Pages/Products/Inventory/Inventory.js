b7a2f299c98dbe7662f3c393c75fe5ca75505b3a
neyaznafiz <niiyazobug@gmail.com>
2022-10-25 12:54:32 +0600

    animation added in inventory section in home

--- src/Component/Pages/Products/Inventory/Inventory.js
+++ src/Component/Pages/Products/Inventory/Inventory.js
@@ -12,10 +12,10 @@ const Inventory = () => {
         <div className='md:card-shadow lg:card-shadow my-4 lg:my-10 lg:mx-auto px-2 md:px-5 lg:px-5  lg:w-11/12'>
 
             <div className='my-4 md:py-14 lg:py-14'>
-                <p className='text-center mx-auto lg:text-4xl font-serif font-semibold border-b-2 border-zinc-700 w-6/12 '>INVENTORY ITEMS</p>
+                <p className='text-center mx-auto lg:text-4xl font-serif font-semibold border-b-2 border-zinc-700 w-6/12 ' data-aos="zoom-in">INVENTORY ITEMS</p>
             </div>
 
-            <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-4'>
+            <div className='grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-4' >
                 {
                     product.slice(0, 4).map(product =>
                         <Product
@@ -26,7 +26,7 @@ const Inventory = () => {
             </div>
 
            <div className='flex justify-end mt-5 pb-4'>
-           <Link to='/manageinventory' className='card-shadow hover:shadow-lg px-3 py-2 flex font-semibold hover:text-black'>MANAGE INVENTORY <MdArrowForwardIos className='mt-1 ml-2'/></Link>
+           <Link to='/manageinventory' className='card-shadow hover:shadow-lg px-3 py-2 flex font-semibold hover:text-black' data-aos="fade-right">MANAGE INVENTORY <MdArrowForwardIos className='mt-1 ml-2'/></Link>
            </div>
 
         </div>
--- src/Component/Pages/Products/Inventory/Product/Product.js
+++ src/Component/Pages/Products/Inventory/Product/Product.js
@@ -1,43 +1,69 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Product = ({ product }) => {
+  const { _id, name, price, img, quantity, supplier, description } = product;
 
-    const { _id, name, price, img, quantity, supplier, description } = product
+  const navigate = useNavigate();
+  const navigateToUpdate = (id) => {
+    navigate(`/update/${_id}`);
+  };
 
-    const navigate = useNavigate()
-    const navigateToUpdate = id => {
-        navigate(`/update/${_id}`)
-    }
+  return (
+    <div className="" data-aos="zoom-in">
+      <div className="flex justify-center ">
+        <div className="flex flex-col md:flex-row lg:w-xl rounded-lg bg-white shadow-lg hover-zoom overflow-hidden">
+          <img
+            className=" w-full h-full lg:h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg hover:rounded-lg"
+            src={img}
+            alt=""
+          />
+          <div className="p-3 md:p-3 lg:p-6 flex flex-col justify-start">
+            <h5
+              className="text-gray-900 text-3xl font-medium mb-1 font-serif"
+              data-aos="fade-left"
+            >
+              {name}
+            </h5>
 
-    return (
-        <div className=''>
+            <div className="">
+              <p
+                className="text-gray-600 lg:text-lg font-bold pb-4"
+                data-aos="fade-left"
+              >
+                Supplier: {supplier}
+              </p>
+              <p className="text-gray-700 text-base mb-4" data-aos="fade-left">
+                {" "}
+                {description.slice(0, 100)}....{" "}
+              </p>
 
-                <div className="flex justify-center ">
-                    <div className="flex flex-col md:flex-row lg:w-xl rounded-lg bg-white shadow-lg hover-zoom overflow-hidden">
-                        <img className=" w-full h-full lg:h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg hover:rounded-lg" src={img} alt="" />
-                        <div className="p-3 md:p-3 lg:p-6 flex flex-col justify-start">
-                            <h5 className="text-gray-900 text-3xl font-medium mb-1 font-serif">{name}</h5>
+              <div className="flex justify-between font-semibold">
+                <p className="text-gray-600" data-aos="fade-left">
+                  Price : ${price}
+                </p>
+                <p className="text-gray-600" data-aos="fade-left">
+                  Quantity : {quantity}
+                </p>
+              </div>
+            </div>
 
-                            <div className=''>
-                                <p className="text-gray-600 lg:text-lg font-bold pb-4">Supplier: {supplier}</p>
-                                <p className="text-gray-700 text-base mb-4"> {description.slice(0, 100)}.... </p>
-
-                                <div className='flex justify-between font-semibold'>
-                                    <p className="text-gray-600">Price : ${price}</p>
-                                    <p className="text-gray-600">Quantity : {quantity}</p>
-                                </div>
-                            </div>
-
-                            <div className='flex justify-end h-full items-end'>
-                                <button onClick={()=>navigateToUpdate(_id)}  className='card-shadow text-center hover:shadow-lg hover:text-black font-semibold px-3 py-2'>UPDATE</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div
+              className="flex justify-end h-full items-end"
+              data-aos="fade-left"
+            >
+              <button
+                onClick={() => navigateToUpdate(_id)}
+                className="card-shadow text-center hover:shadow-lg hover:text-black font-semibold px-3 py-2"
+              >
+                UPDATE
+              </button>
+            </div>
+          </div>
         </div>
-
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Product;
