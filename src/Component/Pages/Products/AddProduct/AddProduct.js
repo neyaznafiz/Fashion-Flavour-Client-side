add8e91b8d209c180797eb3673cf55a3790235a4
neyaznafiz <niiyazobug@gmail.com>
2022-10-25 18:44:38 +0600

    animation added in add product  route

--- src/Component/Pages/Products/AddProduct/AddProduct.js
+++ src/Component/Pages/Products/AddProduct/AddProduct.js
@@ -43,17 +43,22 @@ const AddProduct = () => {
           <button
             onClick={() => navigate(-1)}
             className="form-shadow py-2 px-4 font-semibold"
+            data-aos="fade-left"
           >
             BACK
           </button>
         </div>
 
-        <div className="my-8  flex justify-center">
+        <div className="my-8  flex justify-center" data-aos="zoom-in">
           <form
             onSubmit={handleAddProduct}
             className=" text-white grid lg:w-4/12 gap-4 py-10 px-10 form-shadow"
           >
-            <h2 className="text-center text-4xl font-semibold font-serif">
+            <h2
+              className="text-center text-4xl font-semibold font-serif"
+              data-aos="zoom-in"
+              data-aos-delay="100"
+            >
               ADD YOUR PRODUCT HERE
             </h2>
 
@@ -63,6 +68,7 @@ const AddProduct = () => {
               value={user.email}
               placeholder="EMAIL"
               className="px-2 py-2 rounded-md form-shadow border-0 text-zinc-800 opacity-60 font-semibold"
+              data-aos="flip-down" data-aos-delay="250"
             />
             <input
               type="text"
@@ -70,6 +76,7 @@ const AddProduct = () => {
               placeholder="PRODUCT NAME"
               className=" px-2 py-2 rounded-md form-shadow border-0 text-zinc-800 placeholder-zinc-800/60"
               required
+              data-aos="flip-down" data-aos-delay="250"
             />
             <input
               type="number"
@@ -77,6 +84,7 @@ const AddProduct = () => {
               placeholder="PRODUCT PRICE"
               className=" px-2 py-2 rounded-md form-shadow border-0 text-zinc-800 placeholder-zinc-800/60"
               required
+              data-aos="flip-down" data-aos-delay="250"
             />
             <input
               type="number"
@@ -84,6 +92,7 @@ const AddProduct = () => {
               placeholder="PRODUCT QUANTITY"
               className=" px-2 py-2 rounded-md form-shadow border-0 text-zinc-800 placeholder-zinc-800/60"
               required
+              data-aos="flip-down" data-aos-delay="250"
             />
             <input
               type="text"
@@ -91,6 +100,7 @@ const AddProduct = () => {
               placeholder="SUPPLIER NAME"
               className=" px-2 py-2 rounded-md form-shadow border-0 text-zinc-800 placeholder-zinc-800/60"
               required
+              data-aos="flip-down" data-aos-delay="250"
             />
 
             <input
@@ -99,6 +109,7 @@ const AddProduct = () => {
               placeholder="PRODUCT IMAGE URL"
               className=" px-2 py-2 rounded-md form-shadow border-0 text-zinc-800 placeholder-zinc-800/60"
               required
+              data-aos="flip-down" data-aos-delay="250"
             />
 
             <textarea
@@ -107,12 +118,14 @@ const AddProduct = () => {
               placeholder="WRITE PRODUCT DESCRIPTION"
               className=" px-2 py-2 rounded-md form-shadow border-0 text-zinc-800 placeholder-zinc-800/60"
               required
+              data-aos="flip-down" data-aos-delay="250"
             />
 
             <input
               type="submit"
               value="ADD PRODUCT"
               className=" bg-zinc-800 text-white py-2 rounded-md border-0 w-5/12 mx-auto btn-shadow"
+              data-aos="zoom-in" data-aos-delay="250"
             />
           </form>
         </div>
